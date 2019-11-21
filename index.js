19f4ef4b3ff770f9bf08fd31ad22d4d81f13db82
Miguel Barra <mbarra.4445@gmail.com>
2019-11-21 19:46:26 -0300

    12. Counting

--- index.js
+++ index.js
@@ -32,16 +32,10 @@ async function getCourses() {
  	// and 
 
 	const courses = await Course
-		// .find({ author: 'Mosh', isPublished: true })
-		// Starts with Mosh
-		find({ author: /^Mosh/ })
-		// Ends with Hamedani
-		.find({ author: /Hamedani$/i })
-		// Contains Mosh
-		.find({ author: /.*Mosh.*/ })
+		.find({ author: 'Mosh', isPublished: true })
 		.limit(10)
 		.sort({ name: 1 })
-		.select({ name: 1, tags: 1 });
+		.count();
 	console.log(courses);
 }
 
